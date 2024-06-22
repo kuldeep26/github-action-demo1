16b0ad01119729f72713787bb09cf7900020fe5d
kuldeep26 <kuldeepsingh260893@gmail.com>
2024-06-22 16:53:53 +0530

    adding eks files

--- provider.tf
+++ provider.tf
@@ -1,6 +1,22 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
 terraform {
   backend "s3" {
-    bucket = "test"
+    bucket = "eks-terraform-tfstate-demo"
     key = "services/eks/terraform.tfstate"
+  }
+  required_version = "1.5.5"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.6"
+    }
   }
 }
