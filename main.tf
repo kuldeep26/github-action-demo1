ac4428abce19bb1bf18e6397b0c03a8adebf34c7
kuldeep26 <kuldeepsingh260893@gmail.com>
2024-06-22 12:22:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,20 @@ provider "aws" {
   region  = "us-east-1"
 }
 
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = "my-test-bucket-for-demo"
+resource "aws_s3_bucket" "example" {
+  bucket = "dummy-bucket-to-test-workflow"
+}
+
+resource "aws_s3_bucket_ownership_controls" "example" {
+  bucket = aws_s3_bucket.example.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  depends_on = [aws_s3_bucket_ownership_controls.example]
+
+  bucket = aws_s3_bucket.example.id
   acl    = "private"
 }
