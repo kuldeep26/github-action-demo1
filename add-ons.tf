e64b7247dfd52d5bdc0e7ff192e8416ac0eff6c6
kuldeep26 <kuldeepsingh260893@gmail.com>
2024-07-01 22:01:59 +0530

    fix for tf error

--- add-ons.tf
+++ add-ons.tf
@@ -1,6 +1,27 @@
 resource "aws_eks_addon" "vpc_cni" {
-  cluster_name = aws_eks_cluster.example.name
+  cluster_name = aws_eks_cluster.cluster.name
   addon_name   = "vpc-cni"
   addon_version = "v1.18.2-eksbuild.1"
+  resolve_conflicts_on_update = "PRESERVE"
+}
+
+resource "aws_eks_addon" "vpc_cni" {
+  cluster_name = aws_eks_cluster.cluster.name
+  addon_name   = "kube-proxy"
+  addon_version = "v1.30.0-eksbuild.3"
+  resolve_conflicts_on_update = "PRESERVE"
+}
+
+resource "aws_eks_addon" "vpc_cni" {
+  cluster_name = aws_eks_cluster.cluster.name
+  addon_name   = "aws-ebs-csi-driver"
+  addon_version = "v1.30.0-eksbuild.3"
+  resolve_conflicts_on_update = "PRESERVE"
+}
+
+resource "aws_eks_addon" "vpc_cni" {
+  cluster_name = aws_eks_cluster.cluster.name
+  addon_name   = "coredns"
+  addon_version = "v1.11.1-eksbuild.9"
   resolve_conflicts_on_update = "PRESERVE"
 }
