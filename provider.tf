b346b26f11db34a982a4b94a343284a07338c237
kuldeep26 <kuldeepsingh260893@gmail.com>
2024-07-04 22:09:03 +0530

    fix duplicate provider error

--- provider.tf
+++ provider.tf
@@ -24,17 +24,3 @@ terraform {
     }
   }
 }
-
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.eks.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.eks.token
-}
-
-data "aws_eks_cluster" "eks" {
-  name = "demo"
-}
-
-data "aws_eks_cluster_auth" "eks" {
-  name = data.aws_eks_cluster.eks.name
-}
