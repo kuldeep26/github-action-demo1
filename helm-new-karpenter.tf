566f26523ced84fa42e6e7c507120d9b01588b4a
kuldeep26 <kuldeepsingh260893@gmail.com>
2024-07-06 17:47:10 +0530

    modify module

--- helm-new-karpenter.tf
+++ helm-new-karpenter.tf
@@ -29,7 +29,7 @@ resource "helm_release" "karpenter" {
 
   set {
     name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
-    value = module.karpenter.irsa_arn
+    value = module.karpenter.oidc_provider_arn
   }
 
   set {
