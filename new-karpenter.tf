5e6bf30c44eb48ecb3540f2e0dccefbf1cacc235
kuldeep26 <kuldeepsingh260893@gmail.com>
2024-07-06 12:30:51 +0530

    modify karpenter config

--- new-karpenter.tf
+++ new-karpenter.tf
@@ -1,62 +1,61 @@
-resource "kubectl_manifest" "karpenter_node_class" {
-  yaml_body = <<-YAML
-    apiVersion: karpenter.k8s.aws/v1beta1
-    kind: EC2NodeClass
+# Creating a provisioner which will create additional nodes for unscheduled pods
+resource "kubernetes_manifest" "karpenter_provisioner" {
+  # Terraform by default doesn't tolerate values changing between configuration and apply results.
+  # Users are required to declare these tolerable exceptions explicitly.
+  # With a kubernetes_manifest resource, you can achieve this by using the computed_fields meta-attribute.
+  computed_fields = ["spec.requirements", "spec.limits"]
+  manifest = yamldecode(<<-EOF
+    apiVersion: karpenter.sh/v1alpha5
+    kind: Provisioner
     metadata:
       name: default
     spec:
-      amiFamily: AL2
-      role: ${module.karpenter.node_iam_role_name}
-      subnetSelectorTerms:
-        - tags:
-            karpenter.sh/discovery: ${aws_eks_cluster.cluster.name}
-      securityGroupSelectorTerms:
-        - tags:
-            karpenter.sh/discovery: ${aws_eks_cluster.cluster.name}
-      tags:
-        karpenter.sh/discovery: ${aws_eks_cluster.cluster.name}
-  YAML
+      requirements:
+        - key: karpenter.sh/capacity-type
+          operator: In
+          values: ["on-demand"]
+        - key: "karpenter.k8s.aws/instance-family"
+          operator: In
+          values: ["t3"]
+        - key: karpenter.k8s.aws/instance-size
+          operator: In
+          values: ["medium"]
+      limits:
+        resources:
+          cpu: 1000
+      providerRef:
+        name: default
+      ttlSecondsAfterEmpty: 30
+  EOF
+  )
 
   depends_on = [
     helm_release.karpenter
   ]
 }
 
-resource "kubectl_manifest" "karpenter_node_pool" {
-  yaml_body = <<-YAML
-    apiVersion: karpenter.sh/v1beta1
-    kind: NodePool
+# Creating a Node template, which will be used for Node configuration in AWS side
+resource "kubernetes_manifest" "karpenter_node_template" {
+  # Terraform by default doesn't tolerate values changing between configuration and apply results.
+  # Users are required to declare these tolerable exceptions explicitly.
+  # With a kubernetes_manifest resource, you can achieve this by using the computed_fields meta-attribute.
+  computed_fields = ["spec.requirements", "spec.limits"]
+  manifest = yamldecode(<<-EOF
+    apiVersion: karpenter.k8s.aws/v1alpha1
+    kind: AWSNodeTemplate
     metadata:
       name: default
     spec:
-      template:
-        spec:
-          nodeClassRef:
-            name: default
-          requirements:
-            - key: "karpenter.k8s.aws/instance-category"
-              operator: In
-              values: ["t2", "t3"]
-            # - key: "karpenter.k8s.aws/instance-cpu"
-            #   operator: In
-            #   values: ["2"]
-            # - key: "karpenter.k8s.aws/instance-hypervisor"
-            #   operator: In
-            #   values: ["nitro"]
-            # - key: "karpenter.k8s.aws/instance-generation"
-            #   operator: Gt
-            #   values: ["2"]
-            - key : "karpenter.k8s.aws/instance-size",
-              operator : "In",
-              values : ["medium"]
-      limits:
-        cpu: 1000
-      disruption:
-        consolidationPolicy: WhenEmpty
-        consolidateAfter: 30s
-  YAML
+      subnetSelector:
+        karpenter.sh/discovery: ${aws_eks_cluster.cluster.name}
+      securityGroupSelector:
+        karpenter.sh/discovery: ${aws_eks_cluster.cluster.name}
+      tags:
+        karpenter.sh/discovery: ${aws_eks_cluster.cluster.name}
+  EOF
+  )
 
   depends_on = [
-    kubectl_manifest.karpenter_node_class
+    helm_release.karpenter
   ]
 }
