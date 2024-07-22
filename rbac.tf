f1481ca803fc4cf48ba5656c638668a001827e35
kuldeep26 <kuldeepsingh260893@gmail.com>
2024-07-23 00:40:10 +0530

    modify karpenter manifest

--- rbac.tf
+++ rbac.tf
@@ -4,6 +4,11 @@ provider "kubernetes" {
   host                   = aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(aws_eks_cluster.cluster.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.eks.token
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
+    command     = "aws"
+  }
 }
 
 ##Commenting as below block needed when cluster already running
