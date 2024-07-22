dd1fa04a9868aceffa2b5fa0d142bacd1d7deb12
kuldeep26 <kuldeepsingh260893@gmail.com>
2024-07-22 23:50:55 +0530

    adding karpenter

--- provider.tf
+++ provider.tf
@@ -24,25 +24,25 @@ terraform {
   }
 }
 
-# provider "kubernetes" {
-#   host                   = var.cluster_endpoint
-#   cluster_ca_certificate = base64decode(aws_eks_cluster.cluster.certificate_authority.0.data)
-#   exec {
-#     api_version = "client.authentication.k8s.io/v1beta1"
-#     args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
-#     command     = "aws"
-#   }
-# }
+provider "kubernetes" {
+  host                   = aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(aws_eks_cluster.cluster.certificate_authority.0.data)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
+    command     = "aws"
+  }
+}
 
-# provider "helm" {
-#   kubernetes {
-#     host                   = var.cluster_endpoint
-#     cluster_ca_certificate = base64decode(var.cluster_ca_certificate)
-#     exec {
-#       api_version = "client.authentication.k8s.io/v1beta1"
-#       args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
-#       command     = "aws"
-#     }
-#   }
-#   debug = true
-# }
+provider "helm" {
+  kubernetes {
+    host                   = aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(aws_eks_cluster.cluster.certificate_authority.0.data)
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
+      command     = "aws"
+    }
+  }
+  debug = true
+}
