d2151a95b133694b7e1bd68d80cae5b0bbb932d4
kuldeep26 <kuldeepsingh260893@gmail.com>
2024-07-23 00:37:07 +0530

    modify karpenter manifest

--- provider.tf
+++ provider.tf
@@ -37,12 +37,12 @@ terraform {
 provider "helm" {
   kubernetes {
     host                   = aws_eks_cluster.cluster.endpoint
-    cluster_ca_certificate = base64decode(aws_eks_cluster.cluster.certificate_authority.0.data)
+    cluster_ca_certificate = base64decode(aws_eks_cluster.cluster.certificate_authority[0].data)
+
     exec {
       api_version = "client.authentication.k8s.io/v1beta1"
-      args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
+      args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.cluster.id]
       command     = "aws"
     }
   }
-  debug = true
 }
