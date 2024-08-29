4153993bed5b4d73d2ba746ce880f9a94d290816
kuldeep26 <kuldeepsingh260893@gmail.com>
2024-08-29 21:34:40 +0530

    fix

--- schema-v2/external-secret.tf
+++ schema-v2/external-secret.tf
@@ -7,13 +7,17 @@ resource "helm_release" "external_secret" {
 
 }
 
-locals {
-  rds_secret_arn = aws_db_instance.mydb.master_user_secret[0].secret_arn
+resource "null_resource" "wait_for_rds_secret" {
+  depends_on = [aws_db_instance.mydb]
+
+  provisioner "local-exec" {
+    command = "sleep 120"
+  }
 }
 
 data "aws_secretsmanager_secret" "rds_master_password" {
-  depends_on = [aws_db_instance.mydb]
-  arn = local.rds_secret_arn
+  depends_on = [null_resource.wait_for_rds_secret]
+  arn = aws_db_instance.mydb.master_user_secret[0].secret_arn
 }
 
 
