eec22fe5685bf33dc4aae6233031371325cfbcc1
kuldeep26 <kuldeepsingh260893@gmail.com>
2024-08-29 17:32:59 +0530

    ecr secret

---
+++ schema-v2/ecr.tf
@@ -0,0 +1,14 @@
+data "external" "create_ecr_registry_secret" {
+  program = ["bash", "${path.module}/script/create-ecr-secret.sh"]
+
+  query = {
+    aws_region           = var.aws_region
+    ecr_repository_url   = var.ecr_repository_url
+    namespace            = var.namespace
+  }
+}
+
+variable "ecr_repository_url" {
+  description = "The URL of the ECR repository"
+  default     = "590184049425.dkr.ecr.us-east-1.amazonaws.com"
+}
