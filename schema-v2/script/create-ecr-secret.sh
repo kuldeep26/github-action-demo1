a756b9332e5afaa567b969cdefcfe7c34d759280
kuldeep26 <kuldeepsingh260893@gmail.com>
2024-08-29 17:46:01 +0530

    ecr secret

--- schema-v2/script/create-ecr-secret.sh
+++ schema-v2/script/create-ecr-secret.sh
@@ -3,14 +3,10 @@
 # Assign arguments to variables
 aws_region="$1"
 ecr_repository_url="$2"
-namespace="$3"
+namespace="$(echo "$3" | tr -d '')"
 
 echo "Fetching ECR password"
 ecr_password=$(aws ecr get-login-password --region "$aws_region")
 
 echo "Creating Kubernetes secret"
-kubectl create secret docker-registry ecr-registry-secret \
-  --docker-server="$ecr_repository_url" \
-  --docker-username=AWS \
-  --docker-password="$ecr_password" \
-  --namespace "$namespace"
+kubectl create secret docker-registry ecr-registry-secret --docker-server="$ecr_repository_url" --docker-username=AWS --docker-password="$ecr_password" --namespace "$namespace"
