197b990cc0b184d1a0b3ad0569100b347b659f9a
kuldeep26 <kuldeepsingh260893@gmail.com>
2024-08-29 10:16:46 +0530

    fix

---
+++ schema-v2.tf/schema-service-helm.tf
@@ -0,0 +1,20 @@
+resource "helm_release" "knative_service" {
+  name       = "knative-service"
+  chart      = "./knative-helm-chart"
+  namespace  = var.namespace
+
+  set {
+    name  = "ingress.certificateArn"
+    value = aws_acm_certificate.configurator_cert.arn
+  }
+
+  set {
+    name  = "namespace"
+    value = var.namespace
+  }
+
+  set {
+    name  = "externalSecrets.rdsPasswordKey"
+    value = data.aws_secretsmanager_secret.rds_password_secret.name
+  }
+}
---
+++ schema-v2.tf/variable.tf
@@ -0,0 +1,7 @@
+variable "namespace" {
+  default     = "helloworld"
+}
+
+variable "aws_region" {
+  default     = "us-east-1"
+}
