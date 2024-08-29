9a86dfd97293edc802db2703f7f97094ff91c484
kuldeep26 <kuldeepsingh260893@gmail.com>
2024-08-29 09:15:58 +0530

    fix variable

--- schema-v2/variable.tf
+++ schema-v2/variable.tf
@@ -5,8 +5,3 @@ variable "namespace" {
 variable "aws_region" {
   default     = "us-east-1"
 }
-
-variable "ecr_repository_url" {
-  description = "The URL of the ECR repository"
-  default     = "975050194088.dkr.ecr.us-east-1.amazonaws.com"
-}
