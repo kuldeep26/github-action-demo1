0df9c01b28f4eb480d1dbd54e11dbb348bac2f0a
kuldeep26 <kuldeepsingh260893@gmail.com>
2024-08-29 10:49:09 +0530

    fix

--- schema-v2/ecr.tf
+++ schema-v2/ecr.tf
@@ -1,4 +1,4 @@
-# Use data source for ECR authorization token
+# Fetch ECR authorization token using a data source
 data "aws_ecr_authorization_token" "ecr" {}
 
 resource "kubernetes_secret" "ecr_registry_secret" {
@@ -11,7 +11,7 @@ resource "kubernetes_secret" "ecr_registry_secret" {
     ".dockerconfigjson" = base64encode(jsonencode({
       auths = {
         "${var.ecr_repository_url}" = {
-          auth = base64encode("AWS:${data.aws_ecr_authorization_token.ecr.authorization_token}")
+          auth = data.aws_ecr_authorization_token.ecr.authorization_token
         }
       }
     }))
