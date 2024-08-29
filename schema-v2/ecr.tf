9e4c284a2c119e850730e7c793704864f1e3fbe6
kuldeep26 <kuldeepsingh260893@gmail.com>
2024-08-29 09:33:39 +0530

    fix

--- schema-v2/ecr.tf
+++ schema-v2/ecr.tf
@@ -1,4 +1,4 @@
-resource "aws_ecr_authorization_token" "ecr" {}
+data "aws_ecr_authorization_token" "ecr" {}
 
 resource "kubernetes_secret" "ecr_registry_secret" {
   metadata {
@@ -10,7 +10,7 @@ resource "kubernetes_secret" "ecr_registry_secret" {
     ".dockerconfigjson" = base64encode(jsonencode({
       auths = {
         "${var.ecr_repository_url}" = {
-          auth = base64encode("AWS:${aws_ecr_authorization_token.ecr.password}")
+          auth = base64encode("AWS:${data.aws_ecr_authorization_token.ecr.password}")
         }
       }
     }))
