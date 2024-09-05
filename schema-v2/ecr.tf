c3642763bbb3f4111cca190e9c2872bb0b5c6588
kuldeep26 <kuldeepsingh260893@gmail.com>
2024-09-06 00:35:57 +0530

    ecr image digest

--- schema-v2/ecr.tf
+++ schema-v2/ecr.tf
@@ -12,3 +12,22 @@ variable "ecr_repository_url" {
   description = "The URL of the ECR repository"
   default     = "637423660652.dkr.ecr.us-east-1.amazonaws.com"
 }
+
+///////// Test ///////////
+
+
+# Fetch the ECR repository information
+data "aws_ecr_repository" "example" {
+  name = "ngnix-knative"
+}
+
+# Fetch image information using the image tag (e.g., "latest")
+data "aws_ecr_image" "example" {
+  repository_name = data.aws_ecr_repository.example.name
+  image_tag       = "1.2.1" # You must specify the correct image tag that exists in the repository
+}
+
+# Output the image digest
+output "ecr_image_digest" {
+  value = data.aws_ecr_image.example.image_digest
+}
